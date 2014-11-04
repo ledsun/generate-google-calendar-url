3c6d8e73ed11e753cbc92e96377783c9dc694519
ledsun <shigeru.nakajima@gmail.com>
2014-11-04 10:01:52 +0900

    Be executable in browsers.

--- generate-google-calendar-url.js
+++ generate-google-calendar-url.js
@@ -1,3 +1,14 @@
-module.exports = function() {
+;
+! function(undefined) {
+  var generateUrl = function() {
     return 'http://www.google.com/calendar/event?action=TEMPLATE&text=Title';
-};
+  };
+
+  if (typeof module === 'object') {
+    // CommonJS
+    module.exports = generateUrl;
+  } else {
+    // Browser global.
+    window.generateUrl = generateUrl;
+  }
+}();
