66162a7beae30a7f176ea88f621a6f0c175762e3
ledsun <shigeru.nakajima@gmail.com>
2014-11-04 20:29:00 +0900

    Add a directory "lib".

--- test/generate-google-calendar-url.js
+++ test/generate-google-calendar-url.js
@@ -2,7 +2,7 @@ var assert = require('power-assert');
 
 describe('generate url', function(){
     it('given minimum parameters', function(){
-        var generateUrl = require('../generate-google-calendar-url');
+        var generateUrl = require('../lib/generate-google-calendar-url');
         assert.equal(generateUrl('Title'), 'http://www.google.com/calendar/event?action=TEMPLATE&text=Title');
     });
 });
