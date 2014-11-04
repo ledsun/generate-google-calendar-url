cf182ff91a88db3c1102805646bf38fe5dd06563
ledsun <shigeru.nakajima@gmail.com>
2014-11-04 20:49:08 +0900

    Add a parameter "title".

--- test/generate-google-calendar-url.js
+++ test/generate-google-calendar-url.js
@@ -1,8 +1,26 @@
 var assert = require('power-assert');
 
-describe('generate url', function(){
-    it('given minimum parameters', function(){
-        var generateUrl = require('../lib/generate-google-calendar-url');
-        assert.equal(generateUrl('Title'), 'http://www.google.com/calendar/event?action=TEMPLATE&text=Title');
-    });
+describe('generate url', function() {
+  var generateUrl;
+
+  before(function(done) {
+    generateUrl = require('../lib/generate-google-calendar-url');
+    done();
+  });
+
+  it('given no parameter', function() {
+    assert.equal(generateUrl(), 'http://www.google.com/calendar/event?action=TEMPLATE');
+  });
+
+  it('given multi byte title', function() {
+    assert.equal(generateUrl({
+      title: '新しい予定'
+    }), 'http://www.google.com/calendar/event?action=TEMPLATE&text=%E6%96%B0%E3%81%97%E3%81%84%E4%BA%88%E5%AE%9A');
+  });
+
+  it('given url title', function() {
+    assert.equal(generateUrl({
+      title: 'http://example.com'
+    }), 'http://www.google.com/calendar/event?action=TEMPLATE&text=http%3A%2F%2Fexample.com');
+  });
 });
