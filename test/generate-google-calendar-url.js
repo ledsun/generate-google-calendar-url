2487141f8784fcb5eb782195dbf563962086de56
ledsun <shigeru.nakajima@gmail.com>
2014-11-05 09:40:46 +0900

    Add a parameter "location".

--- test/generate-google-calendar-url.js
+++ test/generate-google-calendar-url.js
@@ -12,15 +12,38 @@ describe('generate url', function() {
     assert.equal(generateUrl(), 'http://www.google.com/calendar/event?action=TEMPLATE');
   });
 
-  it('given multi byte title', function() {
-    assert.equal(generateUrl({
-      title: '新しい予定'
-    }), 'http://www.google.com/calendar/event?action=TEMPLATE&text=%E6%96%B0%E3%81%97%E3%81%84%E4%BA%88%E5%AE%9A');
-  });
+  describe('given title parameter', function() {
+    it('as multi byte', function() {
+      assert.equal(generateUrl({
+        title: '新しい予定'
+      }), 'http://www.google.com/calendar/event?action=TEMPLATE&text=%E6%96%B0%E3%81%97%E3%81%84%E4%BA%88%E5%AE%9A');
+    });
 
-  it('given url title', function() {
+    it('as url', function() {
+      assert.equal(generateUrl({
+        title: 'http://example.com'
+      }), 'http://www.google.com/calendar/event?action=TEMPLATE&text=http%3A%2F%2Fexample.com');
+    });
+  })
+
+  describe('given location parameter', function() {
+    it('as multi byte', function() {
+      assert.equal(generateUrl({
+        location: 'ここではないどこか'
+      }), 'http://www.google.com/calendar/event?action=TEMPLATE&location=%E3%81%93%E3%81%93%E3%81%A7%E3%81%AF%E3%81%AA%E3%81%84%E3%81%A9%E3%81%93%E3%81%8B');
+    });
+
+    it('as url', function() {
+      assert.equal(generateUrl({
+        location: 'http://example.com'
+      }), 'http://www.google.com/calendar/event?action=TEMPLATE&location=http%3A%2F%2Fexample.com');
+    });
+  })
+
+  it('given two parameters', function() {
     assert.equal(generateUrl({
-      title: 'http://example.com'
-    }), 'http://www.google.com/calendar/event?action=TEMPLATE&text=http%3A%2F%2Fexample.com');
-  });
+      title: 'new event',
+      location: 'somewhere'
+  }), 'http://www.google.com/calendar/event?action=TEMPLATE&text=new%20event&location=somewhere');
+  })
 });
